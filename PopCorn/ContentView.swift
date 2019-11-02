d7f8c13ee2bb6f60342cc0460f7ef31770784662
masem <makoto.mase@gmail.com>
2019-11-02 14:53:57 +0900

    Initial Commit

---
+++ PopCorn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PopCorn
+//
+//  Created by 間瀬真 on 2019/11/02.
+//  Copyright © 2019 間瀬真. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
